3a58ac9cab799ce4d4e8b2b62c29fa6033554142
Aleksandr153 <128185370+Aleksandr153@users.noreply.github.com>
2023-04-21 08:36:56 +0300

    Add files via upload

--- Sequence.h
+++ Sequence.h
@@ -11,7 +11,7 @@ using namespace std;
 class Sequence abstract
 {
 public:
-    virtual double getValueOfElement(int& number_element) abstract;
-    virtual void setValueOfElement(int& number_element, double& value_element) abstract;
-    virtual void addValueOfElement(double& value_element) abstract;
+    virtual double getValueOfElement(int& number_element) abstract;                         //получения значения произвольного элемента
+    virtual void setValueOfElement(int& number_element, double& value_element) abstract;    //установки значения произвольного элемента
+    virtual void addValueOfElement(double& value_element) abstract;                         //добавления элемента в конец последовательности
 };
